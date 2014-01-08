e849018f0c1cf8fd8a731c431cf81df0be444eed
seichi <soshiro@sigcomt.com>
2014-01-08 16:15:12 -0500

    Se agrega la libreria jqGrid(js + css) más otras funciones jquery.

---
+++ Administrador de Contenidos/js/BI.js
@@ -0,0 +1,63 @@
+﻿BI = {  
+    ValidarDecimal: function (numero) {
+        var patron = /^([0-9])*[.]?[0-9]*$/;
+        if (patron.test(numero))
+            return true;
+        return false;
+    },
+
+    ValidarFecha: function (fecha) {
+        var patron = /^((([0][1-9]|[12][\d])|[3][01])[-\/]([0][13578]|[1][02])[-\/][1-9]\d\d\d)|((([0][1-9]|[12][\d])|[3][0])[-\/]([0][13456789]|[1][012])[-\/][1-9]\d\d\d)|(([0][1-9]|[12][\d])[-\/][0][2][-\/][1-9]\d([02468][048]|[13579][26]))|(([0][1-9]|[12][0-8])[-\/][0][2][-\/][1-9]\d\d\d)$/;
+        if (patron.test(fecha))
+            return true;
+        return false;
+    },
+
+    ValidarEntero: function (numero) {
+        var patron = /^\d+$/;
+        if (patron.test(numero))
+            return true;
+        return false;
+    },
+
+    ShowAlert: function (dialog, mensaje) {
+        if (dialog == '') {
+            dialog = 'dialog-alert';
+        }
+
+        $('#' + dialog).html("");
+        $('#' + dialog).append("<br/>" + mensaje);
+        $('#' + dialog).dialog("open");
+    },
+    CreateDialogs: function (arrayDialog) {
+
+        for (var i = 0; i < arrayDialog.length; i++) {
+            $("#" + arrayDialog[i].name).dialog({
+                autoOpen: false,
+                resizable: false,
+                closeOnEscape:true,
+                height: arrayDialog[i].height,
+                width: arrayDialog[i].width,
+                title: arrayDialog[i].title,
+                modal: true,
+                open: function () {
+                    //$(this).parent().appendTo($('#aspnetForm'));
+                },
+                close: function () {
+                    var name = $(this).attr('id');
+                    $.each(arrayDialog, function (index, v) {
+                        if (v.name == name) {
+                            if (v.closePopUp != null && typeof (v.closePopUp) == "function") {
+                                v.closePopUp(name);
+                                //return false;
+                            }
+                            else {
+                                $(this).dialog("close");
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    }
+};
