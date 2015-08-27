8368eeadf12161ff2b642d4ca951baf5c51a9670
Luis Alfaro <cjave@sigcomt.com>
2015-08-27 11:18:05 -0500

    Hacer que los log se generen por día con extensión .log.

--- Administrador de Contenidos/App_Code/Log.vb
+++ Administrador de Contenidos/App_Code/Log.vb
@@ -5,22 +5,16 @@ Public Class Log
 
     Public Shared Sub ErrorLog(ByVal mensaje As String)
         Try
-
             Dim directorio As String = AppDomain.CurrentDomain.BaseDirectory & "Log"
-            Dim archivo As String = directorio & "\Log.txt"
-
             If Not Directory.Exists(directorio) Then
                 Directory.CreateDirectory(directorio)
             End If
 
+            Dim archivo As String = directorio & "\" & +Date.Now.Year.ToString("0000") & Date.Now.Month.ToString("00") & Date.Now.Day.ToString("00") + ".log"
             Dim streamWriter As StreamWriter = New StreamWriter(archivo, True)
-
-            streamWriter.WriteLine(Date.Now.ToShortDateString() & " " & Date.Now.ToShortTimeString() & " " & mensaje)
+            streamWriter.WriteLine(Date.Now.ToShortTimeString() & " " & mensaje)
             streamWriter.Close()
-
         Catch ex As Exception
-
-            ErrorLog(mensaje)
 
         End Try
     End Sub
